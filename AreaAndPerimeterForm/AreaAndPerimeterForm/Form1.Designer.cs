9bc05edd8ebf1ba95c2599f93ec1dbe5b5927ad9
NateBigStone <n.gaut@bigstonemgmt.com>
2018-09-29 23:52:23 -0500

    AreaAndPerimeterForm

---
+++ AreaAndPerimeterForm/AreaAndPerimeterForm/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace AreaAndPerimeterForm
+{
+    partial class AreaAndPerimeter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerim = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Length:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Width:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Area:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Perimeter:";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLength.Location = new System.Drawing.Point(139, 29);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(93, 26);
+            this.txtLength.TabIndex = 1;
+            this.txtLength.Click += new System.EventHandler(this.txtLength_Click);
+            this.txtLength.TextChanged += new System.EventHandler(this.txtLength_TextChanged);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWidth.Location = new System.Drawing.Point(139, 65);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(93, 26);
+            this.txtWidth.TabIndex = 2;
+            this.txtWidth.TextChanged += new System.EventHandler(this.txtLength_TextChanged);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(45, 183);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(86, 23);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Enter += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(139, 183);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(93, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnExit_KeyPress);
+            this.btnExit.Leave += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(139, 100);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(93, 23);
+            this.lblArea.TabIndex = 7;
+            // 
+            // lblPerim
+            // 
+            this.lblPerim.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPerim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPerim.Location = new System.Drawing.Point(139, 134);
+            this.lblPerim.Name = "lblPerim";
+            this.lblPerim.Size = new System.Drawing.Size(93, 23);
+            this.lblPerim.TabIndex = 8;
+            // 
+            // AreaAndPerimeter
+            // 
+            this.AcceptButton = this.btnCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblPerim);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AreaAndPerimeter";
+            this.Text = "Area & Perimeter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerim;
+    }
+}
+
---
+++ AreaAndPerimeterForm/AreaAndPerimeterForm/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaAndPerimeterForm
+{
+    public partial class AreaAndPerimeter : Form
+    {
+        public AreaAndPerimeter()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsLengthDecimal(TextBox txtLength, string name)  //Input validation, tests that length is a number
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(txtLength.Text, out number)) 
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a number", "Entry Error"); //If it's NaN, it returns false and shows an error
+                txtLength.Focus();
+                return false;
+            }
+        }
+        public bool IsWidthDecimal(TextBox txtWidth, string name) //tests that width can be converted to a number
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(txtWidth.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a number", "Entry Error");  //returns false and gives an error popup if it's not a number
+                txtWidth.Focus();
+                return false;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)    //exit button closes the app. Esc key access this via form properties 
+        {
+            this.Close();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (IsLengthDecimal(txtLength, "Length") && IsWidthDecimal(txtWidth, "Width"))  //makes sure our tests return true before calculating
+            {
+                decimal length = (decimal)Convert.ToDecimal(txtLength.Text);  //initiates length variable and sets it as the the text entered and converts that text to decimal
+                decimal width = (decimal)Convert.ToDecimal(txtWidth.Text);   //initiates width variable and sets it as the the text entered and converts that text to decimal
+                decimal area = length * width;  //initiates area variable and does calculation
+                decimal perimeter = (length * 2) + (width * 2);  //initiates perimeter variable and does calculations
+                lblArea.Text = Convert.ToString(area);   //converts variable to string and sets the text box to it
+                lblPerim.Text = Convert.ToString(perimeter);  //converts variable to string and sets the text box to it
+            }
+        }
+
+        private void txtLength_TextChanged(object sender, EventArgs e)   //erases results when the length is changed. Width accesses this via form properties
+        {
+            lblArea.Text = "";
+            lblPerim.Text = "";
+        }
+
+        private void txtLength_Click(object sender, EventArgs e)  //disregard
+        {
+        }
+
+        private void btnExit_KeyPress(object sender, KeyPressEventArgs e)   //disregard
+        {
+        }
+    }
+}
