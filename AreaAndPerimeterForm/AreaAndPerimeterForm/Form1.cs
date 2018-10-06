3b54b42dc7de0d36b665caa44a179b8f9562c533
NateBigStone <n.gaut@bigstonemgmt.com>
2018-10-06 16:19:11 -0500

    Length and Perimeter Validations

--- AreaAndPerimeterForm/AreaAndPerimeterForm/Form1.cs
+++ AreaAndPerimeterForm/AreaAndPerimeterForm/Form1.cs
@@ -17,21 +17,55 @@ namespace AreaAndPerimeterForm
             InitializeComponent();
         }
 
-        public bool IsLengthDecimal(TextBox txtLength, string name)  //Input validation, tests that length is a number
+        public bool IsPresent(TextBox textBox, string name)  //input validation, text that textbox isnt blank
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool IsDecimal(TextBox textBox, string name)  //Input validation, tests that the text entered is a number
         {
             decimal number = 0m;
-            if (Decimal.TryParse(txtLength.Text, out number)) 
+            if (Decimal.TryParse(textBox.Text, out number)) 
             {
                 return true;
             }
             else
             {
                 MessageBox.Show(name + " must be a number", "Entry Error"); //If it's NaN, it returns false and shows an error
-                txtLength.Focus();
+                textBox.Focus();
                 return false;
             }
         }
 
+        public bool IsValidData()
+        {
+            if(!IsPresent(txtLength, "Length"))  //tests to ensure a length was entered
+            {
+                return false;
+            }
+            if(!IsDecimal(txtLength, "Length"))  //tests to ensure length is a number
+            {
+                return false;
+            }
+            if (!IsPresent(txtWidth, "Width"))  //tests to ensure a width was entered
+            {
+                return false;
+            }
+            if (!IsDecimal(txtWidth, "Width")) //tests to ensure width is a number
+            {
+                return false;
+            }
+            return true;
+        }
 
         private void btnExit_Click(object sender, EventArgs e)    //exit button closes the app. Esc key access this via form properties 
         {
@@ -42,16 +76,19 @@ namespace AreaAndPerimeterForm
         {
             try  //makes sure our tests return true before calculating
             {
-                decimal length = (decimal)Convert.ToDecimal(txtLength.Text);  //initiates length variable and sets it as the the text entered and converts that text to decimal
-                decimal width = (decimal)Convert.ToDecimal(txtWidth.Text);   //initiates width variable and sets it as the the text entered and converts that text to decimal
-                decimal area = length * width;  //initiates area variable and does calculation
-                decimal perimeter = (length * 2) + (width * 2);  //initiates perimeter variable and does calculations
-                lblArea.Text = Convert.ToString(area);   //converts variable to string and sets the text box to it
-                lblPerim.Text = Convert.ToString(perimeter);  //converts variable to string and sets the text box to it
+                if (IsValidData())
+                {
+                    decimal length = (decimal)Convert.ToDecimal(txtLength.Text);  //initiates length variable and sets it as the the text entered and converts that text to decimal
+                    decimal width = (decimal)Convert.ToDecimal(txtWidth.Text);   //initiates width variable and sets it as the the text entered and converts that text to decimal
+                    decimal area = length * width;  //initiates area variable and does calculation
+                    decimal perimeter = (length * 2) + (width * 2);  //initiates perimeter variable and does calculations
+                    lblArea.Text = Convert.ToString(area);   //converts variable to string and sets the text box to it
+                    lblPerim.Text = Convert.ToString(perimeter);  //converts variable to string and sets the text box to it
+                }
             }
             catch(FormatException)
             {
-                MessageBox.Show("Entries must be numbers", "Entry Error");  //message pops up if entry cant be converted to number
+                MessageBox.Show("Entries must be numbers", "Entry Error");  //redundant but maybe my input validation could miss this
                 txtLength.Focus();
             }
             catch (OverflowException)
@@ -62,7 +99,7 @@ namespace AreaAndPerimeterForm
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, ex.GetType().ToString());  //message pops up for other error
-                txtWidth.Focus();
+                txtLength.Focus();
             }
         }
 
