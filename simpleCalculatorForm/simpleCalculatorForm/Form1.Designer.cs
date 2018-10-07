f78d2ae9bf7d49daaf1bf9fd0582107884b49136
NateBigStone <n.gaut@bigstonemgmt.com>
2018-10-07 01:38:18 -0500

    Simple Calculator

---
+++ simpleCalculatorForm/simpleCalculatorForm/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace simpleCalculatorForm
+{
+    partial class simpleCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOperandOne = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblOperandTwo = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtOperandOne = new System.Windows.Forms.TextBox();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.txtOperandTwo = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperandOne
+            // 
+            this.lblOperandOne.AutoSize = true;
+            this.lblOperandOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperandOne.Location = new System.Drawing.Point(30, 42);
+            this.lblOperandOne.Name = "lblOperandOne";
+            this.lblOperandOne.Size = new System.Drawing.Size(110, 24);
+            this.lblOperandOne.TabIndex = 0;
+            this.lblOperandOne.Text = "Operand 1: ";
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperator.Location = new System.Drawing.Point(46, 83);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(94, 24);
+            this.lblOperator.TabIndex = 0;
+            this.lblOperator.Text = "Operator: ";
+            // 
+            // lblOperandTwo
+            // 
+            this.lblOperandTwo.AutoSize = true;
+            this.lblOperandTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperandTwo.Location = new System.Drawing.Point(30, 122);
+            this.lblOperandTwo.Name = "lblOperandTwo";
+            this.lblOperandTwo.Size = new System.Drawing.Size(110, 24);
+            this.lblOperandTwo.TabIndex = 0;
+            this.lblOperandTwo.Text = "Operand 2: ";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(68, 162);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(72, 24);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "Result: ";
+            // 
+            // txtOperandOne
+            // 
+            this.txtOperandOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperandOne.Location = new System.Drawing.Point(146, 39);
+            this.txtOperandOne.Name = "txtOperandOne";
+            this.txtOperandOne.Size = new System.Drawing.Size(186, 29);
+            this.txtOperandOne.TabIndex = 1;
+            this.txtOperandOne.TextChanged += new System.EventHandler(this.txtOperandOne_TextChanged);
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperator.Location = new System.Drawing.Point(146, 80);
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(32, 29);
+            this.txtOperator.TabIndex = 2;
+            this.txtOperator.TextChanged += new System.EventHandler(this.txtOperandOne_TextChanged);
+            // 
+            // txtOperandTwo
+            // 
+            this.txtOperandTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperandTwo.Location = new System.Drawing.Point(146, 119);
+            this.txtOperandTwo.Name = "txtOperandTwo";
+            this.txtOperandTwo.Size = new System.Drawing.Size(186, 29);
+            this.txtOperandTwo.TabIndex = 3;
+            this.txtOperandTwo.TextChanged += new System.EventHandler(this.txtOperandOne_TextChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(146, 159);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(186, 29);
+            this.txtResult.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(146, 210);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(106, 35);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(268, 210);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(64, 35);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // simpleCalculatorForm
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(370, 282);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOperandTwo);
+            this.Controls.Add(this.txtOperator);
+            this.Controls.Add(this.txtOperandOne);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblOperandTwo);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblOperandOne);
+            this.Name = "simpleCalculatorForm";
+            this.Text = "Simple Calculator";
+            this.Load += new System.EventHandler(this.simpleCalculatorForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOperandOne;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Label lblOperandTwo;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtOperandOne;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.TextBox txtOperandTwo;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ simpleCalculatorForm/simpleCalculatorForm/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simpleCalculatorForm
+{
+    public partial class simpleCalculatorForm : Form
+    {
+        public simpleCalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsPresent(TextBox textBox, string name)  //input validation, text that textbox isnt blank
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool IsDecimal(TextBox textBox, string name)  //Input validation, tests that the text entered is a number
+        {
+            decimal number = 0m;
+            if (Decimal.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a number", "Entry Error"); //If it's not a number, it returns false and shows an error
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name, decimal min, decimal max)  //Input validation, tests that the text is within range
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " + min.ToString() + " and " + max.ToString() + ".", "Entry Error"); //Message box shows if not within range
+                textBox.Focus();
+                return false;
+            }
+            else
+            { 
+                return true;
+            }
+        }
+        public bool IsOperator(TextBox textBox, string name)  // tests that the operator is valid
+        {
+            if(
+                textBox.Text == "+" ||
+                textBox.Text == "-" ||
+                textBox.Text == "*" ||
+                textBox.Text == "/"
+                )
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a +, -, *, or / .", "Entry Error"); //ensures a proper operator was entered
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsValidData()
+        {
+            return
+            //operand 1 tests
+            IsPresent(txtOperandOne, "Operand 1") &&
+            IsDecimal(txtOperandOne, "Operand 1") &&
+            IsWithinRange(txtOperandOne, "Operand 1", 0, 1000000) &&
+            //operand 2 tests
+            IsPresent(txtOperandTwo, "Operand 2") &&
+            IsDecimal(txtOperandTwo, "Operand 2") &&
+            IsWithinRange(txtOperandTwo, "Operand 2", 0, 1000000) &&
+            //operator tests
+            IsPresent(txtOperator, "Operator") &&
+            IsOperator(txtOperator, "Operator");
+        }
+
+
+
+        private void btnExit_Click(object sender, EventArgs e)  //exit button
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)   //calculates the calculations
+        {
+            try
+            {
+                if(IsValidData())                        //runs the validations methods 
+                {
+                    decimal operandOne = Convert.ToDecimal(txtOperandOne.Text);     //creates variables for entries
+                    decimal operandTwo = Convert.ToDecimal(txtOperandTwo.Text);
+                    string operatorVariable = txtOperator.Text;
+                    decimal result = 0;
+                    if (operatorVariable == "+")                                   //all the maths
+                    {
+                        result = operandOne + operandTwo;
+                    }
+                    else if(operatorVariable == "-")
+                    {
+                        result = operandOne - operandTwo;
+                    }
+                    else if (operatorVariable == "*")
+                    {
+                        result = operandOne * operandTwo;
+                    }
+                    else if (operatorVariable == "/")
+                    {
+                        result = operandOne / operandTwo;
+                    }
+                    txtResult.Text = string.Format("{0:f4}", result);
+                }
+            }
+            catch(DivideByZeroException)       //catches a divide by 0
+            {
+                MessageBox.Show("Cannot divide by 0, please enter another value.", "Entry Error");
+                txtOperandTwo.Text = "";
+                txtOperandTwo.Focus();
+            }
+            catch(Exception ex)     //catches all other exceptions
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+                
+            }
+        }
+
+        private void simpleCalculatorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOperandOne_TextChanged(object sender, EventArgs e)  //if text is changed, the result is cleared. The other two textboxes access this function via properties
+        {
+            txtResult.Text = "";
+        }
+    }
+}
