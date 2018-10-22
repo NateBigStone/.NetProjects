2a382f5664bf56069c5901e1671d665075d357f5
NateBigStone <n.gaut@bigstonemgmt.com>
2018-10-21 20:25:54 -0500

    Accumulate Test

---
+++ AccumulateTestScoreData/AccumulateTestScoreData/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace AccumulateTestScoreData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDispUnsort = new System.Windows.Forms.Button();
+            this.btnDispSort = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(81, 51);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(65, 24);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(43, 99);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(103, 24);
+            this.lblTotal.TabIndex = 0;
+            this.lblTotal.Text = "Score total:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(29, 147);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(117, 24);
+            this.lblCount.TabIndex = 0;
+            this.lblCount.Text = "Score count:";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(60, 190);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(86, 24);
+            this.lblAverage.TabIndex = 0;
+            this.lblAverage.Text = "Average:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(152, 46);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(61, 29);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Click += new System.EventHandler(this.txtScore_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(152, 94);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(61, 29);
+            this.txtTotal.TabIndex = 0;
+            this.txtTotal.TabStop = false;
+            // 
+            // txtCount
+            // 
+            this.txtCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCount.Location = new System.Drawing.Point(152, 142);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.ReadOnly = true;
+            this.txtCount.Size = new System.Drawing.Size(61, 29);
+            this.txtCount.TabIndex = 0;
+            this.txtCount.TabStop = false;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAverage.Location = new System.Drawing.Point(152, 190);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(61, 29);
+            this.txtAverage.TabIndex = 0;
+            this.txtAverage.TabStop = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(248, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(132, 29);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDispUnsort
+            // 
+            this.btnDispUnsort.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDispUnsort.Location = new System.Drawing.Point(49, 244);
+            this.btnDispUnsort.Name = "btnDispUnsort";
+            this.btnDispUnsort.Size = new System.Drawing.Size(164, 36);
+            this.btnDispUnsort.TabIndex = 3;
+            this.btnDispUnsort.Text = "Display Unsorted";
+            this.btnDispUnsort.UseVisualStyleBackColor = true;
+            this.btnDispUnsort.Click += new System.EventHandler(this.btnDispUnsort_Click);
+            // 
+            // btnDispSort
+            // 
+            this.btnDispSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDispSort.Location = new System.Drawing.Point(49, 286);
+            this.btnDispSort.Name = "btnDispSort";
+            this.btnDispSort.Size = new System.Drawing.Size(164, 35);
+            this.btnDispSort.TabIndex = 4;
+            this.btnDispSort.Text = "Display Sorted";
+            this.btnDispSort.UseVisualStyleBackColor = true;
+            this.btnDispSort.Click += new System.EventHandler(this.btnDispSort_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(248, 244);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(132, 36);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(248, 286);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(132, 35);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(244, 99);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(93, 24);
+            this.lblMin.TabIndex = 0;
+            this.lblMin.Text = "Minimum:";
+            this.lblMin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(244, 147);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(98, 24);
+            this.lblMax.TabIndex = 8;
+            this.lblMax.Text = "Maximum:";
+            this.lblMax.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMin.Location = new System.Drawing.Point(343, 94);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.ReadOnly = true;
+            this.txtMin.Size = new System.Drawing.Size(37, 29);
+            this.txtMin.TabIndex = 0;
+            this.txtMin.TabStop = false;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMax.Location = new System.Drawing.Point(343, 144);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.ReadOnly = true;
+            this.txtMax.Size = new System.Drawing.Size(37, 29);
+            this.txtMax.TabIndex = 0;
+            this.txtMax.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(450, 353);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDispSort);
+            this.Controls.Add(this.btnDispUnsort);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Score Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDispUnsort;
+        private System.Windows.Forms.Button btnDispSort;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+    }
+}
+
---
+++ AccumulateTestScoreData/AccumulateTestScoreData/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccumulateTestScoreData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)   //disregard
+        {
+
+        }
+
+        List<int> scores = new List<int>();         //creates new list of integers 
+        List<int> sortScores = new List<int>();        // creates a parrallel list of ints to sort 
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();                 //closes form
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsValidData())
+                {
+                    int scoreText = Convert.ToInt32(txtScore.Text);     //creates score int
+                    int total = 0;                                      //initiates total varible
+                    int count = 0;                           //initiates count variable
+                    int average = 0;                                    //initiates average variable
+
+                    scores.Add(scoreText);                              //adds score to list
+                    sortScores.Add(scoreText);                          //adds score to sorted list
+                    SortsTheList();                                     //sorts the sorted list
+
+                    int minimum = sortScores[0];
+                    int maximum = sortScores[(scores.Count - 1)];
+
+                    for (int i = 0; i < scores.Count; i++)              //loop to add all elements in list
+                    {
+                        total += scores[i];
+                    }
+
+                    count = scores.Count;
+                    average = total / count;                           //math for average
+
+                    txtTotal.Text = Convert.ToString(total);            //displays total
+                    txtCount.Text = Convert.ToString(count);            //displays Count
+                    txtAverage.Text = Convert.ToString(average);        //displays average
+                    txtMin.Text = Convert.ToString(minimum);            //displays min
+                    txtMax.Text = Convert.ToString(maximum);            //displays max
+                    txtScore.Text = "";                                 //clears score
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message,
+                    ex.GetType().ToString()
+                    );
+            }
+        }
+
+        private void txtScore_Click(object sender, EventArgs e)
+        {
+            txtScore.Text = "";
+        }
+
+        private void btnDispUnsort_Click(object sender, EventArgs e)
+        {
+            DisplayScores(scores, "Unsorted Scores");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtScore.Text = "";                //clears the display and lists
+            txtTotal.Text = "";
+            txtCount.Text = "";
+            txtAverage.Text = "";
+            txtMin.Text = "";
+            txtMax.Text = "";
+            scores.Clear();
+            sortScores.Clear();
+        }
+
+        private void btnDispSort_Click(object sender, EventArgs e)
+        {
+            SortsTheList();
+            DisplayScores(sortScores, "Sorted Scores");
+        }
+
+        public void DisplayScores(List<int> scoreList, string name) // displays scores
+        {
+            string scoreDisplay = "";
+            for (int j = 0; j < scoreList.Count; j++)     //loop to add all elements in list
+            {
+                scoreDisplay += Convert.ToString(scoreList[j]) + "\n"; 
+            }
+            MessageBox.Show(scoreDisplay, name);
+        }
+
+        public void SortsTheList()
+        {
+            sortScores.Sort();
+        }
+
+        public bool IsPresent(TextBox textBox, string name)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsInt(TextBox textBox, string name)
+        {
+            int number = 0;
+            if(Int32.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a decimal value.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+
+        public bool IsWithinRange(TextBox textBox, string name, int min, int max)
+        {
+            int number = Convert.ToInt32(textBox.Text);
+            if(number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " + min.ToString() + " and " + max.ToString() + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsValidData()
+        {
+            return
+                IsPresent(txtScore, "Score") &&
+                IsInt(txtScore, "Score") &&
+                IsWithinRange(txtScore, "Score", 0, 100);
+        }
+
+        private void label1_Click(object sender, EventArgs e)   //disregard
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)       //disregard
+        {
+
+        }
+    }
+}
